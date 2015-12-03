4c60849dcb96424b22a3cf4123ea6f0ebfcd34fc
Zishan Khan <zishan@Zishans-MacBook-Pro.local>
2015-12-03 12:52:45 +0100

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name comicbooksApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the comicbooksApp
+ */
+angular.module('comicbooksApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name comicbooksApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the comicbooksApp
+ */
+angular.module('comicbooksApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
